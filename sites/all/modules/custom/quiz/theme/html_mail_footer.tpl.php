117bb4142f712029846f447c4e28088bef13a869
rramos78 <[rramos@bilbomatica.es]>
2020-03-18 13:20:11 +0100

    Fix template

--- sites/all/modules/custom/quiz/theme/html_mail_footer.tpl.php
+++ sites/all/modules/custom/quiz/theme/html_mail_footer.tpl.php
@@ -1,93 +1,32 @@
-<table border="0" cellpadding="0" cellspacing="0" style="width:800px;margin-top:3em;height: 170px;" class="template-container newsletter-container newsletter-footer">
+<table border="0" cellpadding="0" cellspacing="0" style="width:700px;margin-top:3em" class="template-container newsletter-container newsletter-footer">
   <tbody>
     <tr>
       <?php
         global $base_url;
         $bg_path =  $base_url . "/sites/all/themes/bootstrapDs/images/imghwccrm/";
       ?>
-      <td  bgcolor="#a6be1d" valign="top">
-        <!--[if gte mso 9]>
-          <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="mso-width-percent:1000;">
-            <v:fill type="frame" src="<?php print $bg_path; ?>" color="#416400" />
-            <v:textbox style="mso-fit-shape-to-text:true" inset="0,0,0,0">
-        <![endif]-->
-        <div>
-      <!-- Footer -->
-       <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family:Arial,sans-serif;font-size:14px;border:0;margin:0;border-collapse:collapse;border-spacing:0;background-color:#ffffff;table-layout:fixed;max-width:800px;width:800px">
-          <tbody>
-             <tr style="border:0;background-color:transparent">
-                <td style="vertical-align:top;border:0;">
-                   <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family:Arial,sans-serif;font-size:14px;border:0;margin:0;border-collapse:collapse;border-spacing:0">
-                      <tbody>
-                         <tr style="border:0;background-color:#a6be1d;height:100px">
-                            <td style="vertical-align:middle;border:0;color:#ffffff;padding:0">
-                               <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family:Arial,sans-serif;font-size:14px;border:0;margin:0;border-collapse:collapse;border-spacing:0">
-                                  <tbody>
-                                     <tr style="border:0;background-color:transparent">
-                                        <td style="vertical-align:top;border:0;padding:5px 20px 5px 20px;width:150px">
-                                           &nbsp;
-                                        </td>
-                                        <td style="vertical-align:top;border:0;padding:5px 20px 5px 20px;width:60px;text-align:center;padding-top:5px">
-                                           <a href="https://twitter.com/eu_osha" style="color:#0074bd;text-decoration:none;display:inline-block;vertical-align:middle" target="_blank" data-saferedirecturl="https://twitter.com/eu_osha&amp;source=gmail&amp;ust=1581508467712000&amp;usg=AFQjCNEjD-bso_INECdN2hSssHEjnJUtMA">
-                                              <img style="display:inline;vertical-align:middle;border:0px;height:25px;max-height:25px;width:31px;max-width:31px" src="<?php print $bg_path; ?>twitter.jpg" width="31" height="25" alt="Twitter" class="CToWUd">
-                                           </a>
-                                        </td>
-                                        <td style="vertical-align:top;border:0;padding:5px 20px 5px 20px;width:60px;text-align:center;padding-top:5px">
-                                           <a href="https://www.facebook.com/EuropeanAgencyforSafetyandHealthatWork" style="color:#0074bd;text-decoration:none;display:inline-block;vertical-align:middle" target="_blank" data-saferedirecturl="https://www.facebook.com/EuropeanAgencyforSafetyandHealthatWork&amp;source=gmail&amp;ust=1581508467712000&amp;usg=AFQjCNE8B4kEJTEXOwpWLsxNQLlY3qVc8A">
-                                              <img style="display:inline;vertical-align:middle;border:0px;height:25px;max-height:25px;width:25px;max-width:25px" src="<?php print $bg_path; ?>facebook.jpg" width="25" height="25" alt="Facebook" class="CToWUd">
-                                           </a>
-                                        </td>
-                                        <td style="vertical-align:top;border:0;padding:5px 20px 5px 20px;width:60px;text-align:center;padding-top:5px">
-                                           <a href="https://www.linkedin.com/company/european-agency-for-safety-and-health-at-work" style="color:#0074bd;text-decoration:none;display:inline-block;vertical-align:middle" target="_blank" data-saferedirecturl="https://www.linkedin.com/company/european-agency-for-safety-and-health-at-work&amp;source=gmail&amp;ust=1581508467712000&amp;usg=AFQjCNECEeZnqSz65ut3caY8dHk2QulewA">
-                                              <img style="display:inline;vertical-align:middle;border:0px;height:25px;max-height:25px;width:25px;max-width:25px" src="<?php print $bg_path; ?>linkedin.jpg" width="25" height="25" alt="LinkedIn" class="CToWUd">
-                                           </a>
-                                        </td>
-                                        <td style="vertical-align:top;border:0;padding:5px 20px 5px 20px;width:60px;text-align:center;padding-top:5px">
-                                           <a href="https://www.youtube.com/user/EUOSHA" style="color:#0074bd;text-decoration:none;display:inline-block;vertical-align:middle" target="_blank" data-saferedirecturl="https://www.youtube.com/user/EUOSHA&amp;source=gmail&amp;ust=1581508467712000&amp;usg=AFQjCNF2DU3gB_j1-A3zLqUuB5pY10OCZQ">
-                                              <img style="display:inline;vertical-align:middle;border:0px;height:25px;max-height:25px;width:35px;max-width:35px" src="<?php print $bg_path; ?>youtube.jpg" width="35" height="25" alt="Youtube" class="CToWUd">
-                                           </a>
-                                        </td>
-                                        <td style="vertical-align:top;border:0;padding:5px 20px 5px 20px;width:60px;text-align:center;padding-top:5px">
-                                           <a href="https://www.flickr.com/photos/euosha/albums" style="color:#0074bd;text-decoration:none;display:inline-block;vertical-align:middle" target="_blank" data-saferedirecturl="https://www.flickr.com/photos/euosha/albums&amp;source=gmail&amp;ust=1581508467712000&amp;usg=AFQjCNHzz7YT4uQIFOXGOqk5lcjUCxlaeg">
-                                              <img style="display:inline;vertical-align:middle;border:0px;height:25px;max-height:25px;width:31px;max-width:31px" src="<?php print $bg_path; ?>flickr.jpg" width="31" height="25" alt="Flickr" class="CToWUd">
-                                           </a>
-                                        </td>
-                                        <td style="vertical-align:top;border:0;padding:5px 20px 5px 20px;width:150px">
-                                           &nbsp;
-                                        </td>
-                                     </tr>
-                                  </tbody>
-                               </table>
-                            </td>
-                         </tr>
-                      </tbody>
-                   </table>
-                   <table style="font-family:Arial,sans-serif;font-size:14px;border:0;margin:0;border-collapse:collapse;border-spacing:0;padding-top:15px;width:700px;margin-left:50px">
-                      <tbody>
-                         <tr style="border:0;background-color:transparent">
-                            <td style="vertical-align:top;border:0;padding:5px 20px 5px 20px;padding-bottom:20px;font-size:10px;font-family:verdana;color:#666666;text-align:center">
-                               This message is intended exclusively for its addressee and may contain information that is CONFIDENTIAL and protected by professional privilege. If you are not the intended recipient you are hereby notified that any dissemination, copy or disclosure of this communication is strictly prohibited by law. If this message has been received in error, please immediately notify us via e-mail and delete it.          
-                            </td>
-                         </tr>
-                      </tbody>
-                   </table>
-                </td>
-             </tr>
-          </tbody>
+      <td>
+        <table class="deviceWidth" bgcolor="#ffffff" width="700" border="0" align="center" cellpadding="0" cellspacing="0" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;">
+                  <tr>
+                     <td width="700" ><img class="img_scale" src="<?php print $bg_path; ?>corpfooter.jpg" alt="Healthy Workplaces" title="Healthy Workplaces" width="700" height="112" border="0" style="display: inline-block;" /></td>
+                  </tr>
+               </table>
+      <table class="deviceWidth" bgcolor="#ffffff" width="700" height="10" border="0" align="center" cellpadding="0" cellspacing="0" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;">
+         <tr>
+           <td width="700" height="10" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;line-height: 10px;mso-line-height-rule: exactly;">&nbsp;</td>
+         </tr>
        </table>
-
-       <!-- END Footer -->
-        </div>
-        <!--[if gte mso 9]>
-          </v:textbox>
-        </v:rect>
-        <![endif]-->
+       <table class="table-inner" width="94%" border="0" align="center" cellpadding="0" cellspacing="0" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;">
+         <tr>
+           <td width="100%" align="left" valign="top" class="block_td percent_td"><p style="margin:0 0 10px !important; color:#757575; font-weight:200; font-family: Arial, Helvetica, sans-serif; font-size:8px; line-height: 20px;mso-line-height-rule: exactly;">The information contained in this message and attachments is intended solely for the attention and use of the named addressee and may be confidential. If you are not the intended recipient, you are notified that any dissemination of this communication is strictly prohibited. If you have received this communication in error, please notify us immediately by return of this e-mail. This communication does not constitute any formal commitment on behalf of the European Agency for Safety and Health at Work. The integrity of this message sent over the internet cannot be guaranteed and therefore the European Agency for Safety and Health at Work will accept no liability for any improper functioning of the systems involved.</p></td>
+         </tr>
+       </table>
+       <table class="deviceWidth" bgcolor="#ffffff" width="700" height="10" border="0" align="center" cellpadding="0" cellspacing="0" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;">
+         <tr>
+           <td width="700" height="10" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;line-height: 10px;mso-line-height-rule: exactly;">&nbsp;</td>
+         </tr>
+      </table>
       </td>
     </tr>
   </tbody>
 </table>
-<div class="gmailfix" style="white-space:nowrap; font:15px courier; line-height:0;">
-  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-</div>
